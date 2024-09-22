8b49fa8a7eccd0e8b152c18a8b3d38b4a4693c47
mhandanin <mhandanin@gmail.com>
2024-09-22 16:39:37 +0100

    add flags

--- app/src/main/java/com/bahraoui/convertisseurunits/MainActivity.kt
+++ app/src/main/java/com/bahraoui/convertisseurunits/MainActivity.kt
@@ -7,6 +7,7 @@ import android.widget.TextView
 import android.widget.Button
 import android.widget.Toast
 import android.widget.EditText
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -23,7 +24,8 @@ class MainActivity : AppCompatActivity() {
         val monnaie_to=findViewById<Spinner>(R.id.monnaie_to)
         val result=findViewById<TextView>(R.id.result)
         val convertBTN=findViewById<Button>(R.id.convertBTN)
-
+        val from_flag=findViewById<ImageView>(R.id.from_flag)
+        val to_flag=findViewById<ImageView>(R.id.to_flag)
 
 
         convertBTN.setOnClickListener{
@@ -36,25 +38,78 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this, "Please enter a valid number", Toast.LENGTH_LONG).show()
                 return@setOnClickListener
             }
-
+            var resultat=0.0
            val convertedCurrency=when(M_from to T_from){
-               "MAD" to "Euro" ->  currencyInput*10.8097
-               "MAD" to "US Dollar" -> currencyInput*9.68187
-               "MAD" to "Rouble" -> currencyInput*0.104340
-               "Euro" to "MAD" -> currencyInput*0.0925134
-               "Euro" to "US Dollar" -> currencyInput* 0.895732
-               "Euro" to "Rouble" -> currencyInput*0.00965476
-               "US Dollar" to "MAD" -> currencyInput*0.103279
-               "US Dollar" to "Euro" -> currencyInput*1.11631
-               "US Dollar" to "Rouble" -> currencyInput*0.0107773
-               "Rouble" to "MAD" -> currencyInput*0.104362
-               "Rouble" to "Euro" -> currencyInput*0.00965563
-               "Rouble" to "US Dollar" -> currencyInput*0.0107779
+               "MAD" to "Euro" -> {
+                   resultat = currencyInput * 0.0925341
+                   from_flag.setImageResource(R.drawable.moroccan_flag)
+                   to_flag.setImageResource(R.drawable.eu_icon)
+               }
+               "MAD" to "US Dollar" -> {
+                   resultat = currencyInput*0.103289
+                   from_flag.setImageResource(R.drawable.moroccan_flag)
+                   to_flag.setImageResource(R.drawable.us_icon)
+               }
+               "MAD" to "Rouble" -> {
+                   resultat = currencyInput*9.55764
+                   from_flag.setImageResource(R.drawable.moroccan_flag)
+                   to_flag.setImageResource(R.drawable.russia_icon)
+               }
 
-               else -> {Toast.makeText(this,"Please choose a currency!",Toast.LENGTH_LONG).show()}
+               "Euro" to "MAD" -> {
+                   resultat = currencyInput*10.8068
+                   from_flag.setImageResource(R.drawable.eu_icon)
+                   to_flag.setImageResource(R.drawable.moroccan_flag)
+               }
+               "Euro" to "US Dollar" -> {
+                   resultat = currencyInput* 1.11623
+                   from_flag.setImageResource(R.drawable.eu_icon)
+                   to_flag.setImageResource(R.drawable.us_icon)
+               }
+               "Euro" to "Rouble" -> {
+                   resultat = currencyInput*103.288
+                   from_flag.setImageResource(R.drawable.eu_icon)
+                   to_flag.setImageResource(R.drawable.russia_icon)
+               }
+               "US Dollar" to "MAD" -> {
+                   resultat = currencyInput*0.103279
+                   from_flag.setImageResource(R.drawable.us_icon)
+                   to_flag.setImageResource(R.drawable.moroccan_flag)
+               }
+               "US Dollar" to "Euro" -> {
+                   resultat = currencyInput*1.11631
+                   from_flag.setImageResource(R.drawable.us_icon)
+                   to_flag.setImageResource(R.drawable.eu_icon)
+               }
+               "US Dollar" to "Rouble" -> {
+                   resultat = currencyInput*92.5331
+                   from_flag.setImageResource(R.drawable.us_icon)
+                   to_flag.setImageResource(R.drawable.russia_icon)
+               }
+               "Rouble" to "MAD" -> {
+                   resultat = currencyInput*0.104628
+                   from_flag.setImageResource(R.drawable.russia_icon)
+                   to_flag.setImageResource(R.drawable.moroccan_flag)
+               }
+               "Rouble" to "Euro" -> {
+                   resultat = currencyInput*0.00965563
+                   from_flag.setImageResource(R.drawable.russia_icon)
+                   to_flag.setImageResource(R.drawable.eu_icon)
+               }
+               "Rouble" to "US Dollar" -> {
+                   resultat= currencyInput*0.0107779
+                   from_flag.setImageResource(R.drawable.russia_icon)
+                   to_flag.setImageResource(R.drawable.us_icon)
+               }
+
+               else -> null
            }
 
-        result.text="Result: $convertedCurrency"
+            if (convertedCurrency != null) {
+                result.text = "Result: $resultat"
+            } else {
+                Toast.makeText(this, "Please choose a valid currency pair!", Toast.LENGTH_LONG).show()
+            }
 
         }
 
